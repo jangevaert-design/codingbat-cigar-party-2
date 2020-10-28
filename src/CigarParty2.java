ab30298592a06bb78163f7c656408a9c7d855b57
Jan Gevaert <jan.gevaert@gmail.com>
2020-10-28 05:36:03 -0600

    Initial commit

---
+++ src/CigarParty2.java
@@ -0,0 +1,6 @@
+public class CigarParty2 {
+  public boolean cigarParty(int cigars, boolean isWeekend) {
+    return ((cigars >= 40 && cigars <= 60 && !isWeekend) ||
+        (cigars >= 40 && isWeekend));
+  }
+}
